9c0428c934f3c9c2277a09efeb1a05fadbb55349
vattgern <sashawot060@gmail.com>
2023-09-11 17:04:54 +0300

    theme add

--- js/theme.js
+++ js/theme.js
@@ -10,7 +10,17 @@ console.log(theme.getAttribute('data-theme'));
 btn.addEventListener("click", () => {
     if(theme.getAttribute('data-theme') == 'dark'){
         theme.setAttribute('data-theme', 'light');
+
+        document.querySelector("#svgEmail").setAttribute("stroke", "black");
+        document.querySelector("#svgCV").setAttribute("stroke", "black");
+        document.querySelector("#svgTelegram").setAttribute("fill","black");
+        document.querySelector("#svgGithub").setAttribute("fill","black");
     } else {
         theme.setAttribute('data-theme', 'dark');
+
+        document.querySelector("#svgEmail").setAttribute("stroke", "white");
+        document.querySelector("#svgCV").setAttribute("stroke", "white");
+        document.querySelector("#svgTelegram").setAttribute("fill","white");
+        document.querySelector("#svgGithub").setAttribute("fill","white");
     }
 })
